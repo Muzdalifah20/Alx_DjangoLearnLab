fedbed021b00b5f320dfb077a5182ad114bb85d5
Muzdalifah <muzdalifahsuliman@gmail.com>
2025-09-07 13:04:11 +0300

    exercise model relationship

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,9 +1,9 @@
 from .models import Author, Book, Library, Librarian
 
-def books_by_author(author_name):
+def books_by_author(author):
     try:
-        author = Author.objects.get(name=author_name)
-        books = author.books.all()
+         
+        books = Book.objects.filter(author=author)
         return books
     except Author.DoesNotExist:
         return []
