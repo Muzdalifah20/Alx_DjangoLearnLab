41d31048d1af028e67c09847a374578e51697c13
Muzdalifah <muzdalifahsuliman@gmail.com>
2025-09-07 13:46:21 +0300

    Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from .models import Book, Library
+from .models import Library, Book, Author, Librarian 
 
 # Create your views here.
 
