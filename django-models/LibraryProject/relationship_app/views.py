21fb983b9e8816c2c1aa39fc4d723da6eac0c09e
Muzdalifah <muzdalifahsuliman@gmail.com>
2025-09-07 15:40:02 +0300

    Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -44,19 +44,23 @@ def register(request):
     context = {'form': form}
     return render(request, 'relationship_app/register.html', context)
 
-def check_role(role):
-    def decorator(user):
-        return hasattr(user, 'userprofile') and user.userprofile.role == role
-    return user_passes_test(decorator)
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
-@check_role('Admin')
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
-@check_role('Librarian')
+@user_passes_test(is_librarian)
 def librarian_view(request):
     return render(request, 'relationship_app/librarian_view.html')
 
-@check_role('Member')
+@user_passes_test(is_member)
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
