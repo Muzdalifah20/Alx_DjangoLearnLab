c3c0830b919ddc1921a90a9e04f36b74f8f757ee
Muzdalifah <muzdalifahsuliman@gmail.com>
2025-09-14 15:11:00 +0300

    security

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from .models import Book
 
+# This view lists books filtered by query parameter 'q' if provided
+# Only accessible by users with 'can_view' permission in 'bookshelf' app
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
-    # books = Book.objects.all()
-    books = Book.objects.filter(title__icontains=request.GET.get('q', ''))
+    # Get search query from URL ?q= parameter, default empty string means all books
+    query = request.GET.get('q', '')
+    # Filter Books whose title contains the query string (case-insensitive)
+    books = Book.objects.filter(title__icontains=query)
+    # Render the book list template and send 'books' as context
     return render(request, 'bookshelf/book_list.html', {'books': books})
