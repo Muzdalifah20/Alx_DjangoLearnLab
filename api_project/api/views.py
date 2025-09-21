83fd0378bb662197cb6b7facf19687bf82905638
muzdalifah <muzdalifahsuliman@gmail.com>
2025-09-21 14:27:35 +0300

    permession and authenticathion

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
@@ -9,3 +11,5 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
