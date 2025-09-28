f51a12fbf08a68695ead6fe09990aef07737fa3c
Muzdalifah <muzdalifahsuliman@gmail.com>
2025-09-28 15:43:18 +0300

    Implementing Filtering, Searching, and Ordering in Django REST Framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,5 @@
- 
 from rest_framework import generics, permissions, filters
 from django_filters.rest_framework import DjangoFilterBackend
-
 from .models import Book
 from .serializers import BookSerializer
 
