a8261dc40ac4995fa07f2098828394fc821ca7bb
Muzdalifah <muzdalifahsuliman@gmail.com>
2025-10-05 16:02:02 +0300

    Implementing the Blog's User Authentication System

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationFor, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Profile
+
  
 
 def register(request):
@@ -15,17 +18,23 @@ def register(request):
 
 @login_required
 def profile(request):
+    user = request.user
+    try:
+        profile = user.profile
+    except ObjectDoesNotExist:
+        profile = Profile.objects.create(user=user)  # create profile if missing
+
     if request.method == "POST":
-        updated_form = UserUpdateForm(request.POST, instance=request.user)
-        profile_form = ProfileUpdateForm(request.POST, request.FILES,instance=request.user.profile)
+        updated_form = UserUpdateForm(request.POST, instance=user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=profile)
 
         if updated_form.is_valid() and profile_form.is_valid():
             updated_form.save()
             profile_form.save()
             return redirect('profile')
     else:
-        updated_form = UserUpdateForm(instance=request.user)
-        profile_form = ProfileUpdateForm(instance=request.user.profile)
-    
-    context = {'updated_form': updated_form, "profile_form": profile_form }
+        updated_form = UserUpdateForm(instance=user)
+        profile_form = ProfileUpdateForm(instance=profile)
+
+    context = {'updated_form': updated_form, 'profile_form': profile_form}
     return render(request, 'profile.html', context)
